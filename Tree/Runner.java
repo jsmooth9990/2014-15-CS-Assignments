884817e0464e1da0874e33477732791cf0c86ea2
lemonFlute <wzli98@gmail.com>
2016-01-02 23:33:37 -0500

    Begins new instance of "20 Questions"

---
+++ Tree/Runner.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class Runner 
+{
+	public static void main(String[] args)
+	{	
+		Tree mytree = new Tree();
+		mytree.game(); //run the 20 questions game
+	
+		
+	}
+	
+}
